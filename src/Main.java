df455055dcfe8b5e73130dc5ff91615f86ba38bf
Oksana <oksana017m@mail.com>
2023-02-19 13:19:17 +0300

    add src/Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice  = 2_328;
+
+        int bonusMile = ticketPrice / 20;
+        System.out.println("Бонусных миль: " + bonusMile);
+    }
+}
